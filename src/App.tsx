310a913e1fda9f379519272cb6a370b2747d794f
Anartz Mugika Ledo <mugan86@gmail.com>
2022-12-19 20:08:13 +0100

    feat: load map with basic config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
+import { Map, tileLayer } from 'leaflet';
 import './App.css';
 
 function App() {
+  const [map, setMap] = useState<Map>();
+  const mapInit = useRef<boolean>(false);
+
+  const initMap = () => {
+    tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map!);
+  }
+  useEffect(() => {
+    if (!mapInit.current) {
+      mapInit.current = true;
+      setMap(
+        new Map('map', {
+          center: [-33.8678, 151.21],
+          zoom: 15,
+        }).setView([-33.8678, 151.21])
+      )
+    }
+    if (map) {
+      initMap();
+    }
+  }, [mapInit, map ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="map"></div>
   );
 }
 
